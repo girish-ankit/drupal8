ff4ccc264340a4c1a65def5a1888c1c469114dcc
girish-ankit <manabendra.sarkar@hytechpro.com>
2017-03-27 18:19:45 +0530

    how to call theme function to create template for our data

--- modules/custom/mymodule/src/Controller/FirstController.php
+++ modules/custom/mymodule/src/Controller/FirstController.php
@@ -12,9 +12,19 @@ use Drupal\Core\Controller\ControllerBase;
 class FirstController extends ControllerBase {
 
     public function content() {
+
+	$users = array(
+	    array('name' => 'Ankit', age => 31),
+	    array('name' => 'Amrit', age => 1),
+	    array('name' => 'Raj Kiran', age => 26),
+	);
+
+	// demo_template is theme function key used in hook_theme
+	// test_var is variable name that is used in template file
+
 	return array(
-	    '#type' => 'markup',
-	    '#markup' => t('This is my menu linked custom page'),
+	    '#theme' => 'demo_template',
+	    '#test_var' => $users,
 	);
     }
 
